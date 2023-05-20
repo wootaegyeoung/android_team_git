8830d1d5c8168e3570d3ab08e2e2809034f155db
jhk01007 <75934142+jhk01007@users.noreply.github.com>
2023-05-20 19:52:13 +0900

    로그인 회원가입 파이어베이스 연동 완료

---
+++ app/src/main/java/com/gachon/mp_termproject/UserAccount.java
@@ -0,0 +1,53 @@
+package com.gachon.mp_termproject;
+
+public class UserAccount {
+
+    public UserAccount() {
+
+    }
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken){
+        this.idToken = idToken;
+    }
+    private String idToken;
+
+    // 이메일
+    public String getEmailId() {
+        return emialId;
+    }
+    public void setEmailId(String emailId) {
+        this.emialId = emailId;
+    }
+    private String emialId;
+
+    // 비번
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    private String password;
+
+    // 이름
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    private String name;
+
+    // 전화번호
+    public String getPhone(){
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    private String phone;
+
+}
---
+++ app/src/main/java/com/gachon/mp_termproject/login.java
@@ -0,0 +1,77 @@
+package com.gachon.mp_termproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class login extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth; // 파이어 베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터 베이스
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        ImageButton btnLogin = findViewById(R.id.btn_login);
+        TextView signUp = findViewById(R.id.tv_signup);
+
+        EditText login_email = findViewById(R.id.login_edit_email);
+        EditText login_password = findViewById(R.id.login_edit_password);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), signup.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strEmail = login_email.getText().toString();
+                String strPwd = login_password.getText().toString();
+
+                if(strEmail.equals(""))
+                    strEmail = "****";
+                if(strPwd.equals(""))
+                    strPwd = "++++";
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            // 로그인 성공
+                            Intent intent = new Intent(login.this, MainActivity.class);
+                            startActivity(intent);
+                            login_email.setText("");
+                            login_password.setText("");
+                        }else {
+                            Toast.makeText(login.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+}
